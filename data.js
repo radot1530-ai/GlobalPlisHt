ab734996995daa5f4d961d5138a3d920fcb576dc
radot1530-ai <radot1530@gmail.com>
2025-12-14 16:18:52 -0500

    Refactor data.js with updated listings and properties
    Updated property values and added new entries for kayList, terresList, and servicesList. Fixed inconsistencies in IDs and image paths.

--- data.js
+++ data.js
@@ -1,11 +1,52 @@
 // data.js
 
 const kayList = [
+
   {
-    id: 1,
-    tit: "Kay modèn nan Delmas",
+    
+    id: 2,
+    tit:"",
+    pri:"",
+    adresse:"",
+    deskripsyon: "",
+    imaj:"/image/Screenshot_20251214-145554.png",
+    adminwhatsapp: "Ajoute Anons"
+  },
+   { id: 2,
+    tit:"",
+    pri:"",
+    adresse:"",
+    deskripsyon: "À louer",
+    imaj:"/image/Screenshot_20251214-152535.png",
+    adminwhatsapp: "Achte/Negosye"
+  },
+    {
+    
+    id: 2,
+    tit:"",
+    pri:"",
+    adresse:"",
+    deskripsyon: "À louer",
+    imaj:"/image/Screenshot_20251214-152429.png",
+    adminwhatsapp: "Achte/Negosye"
+  },
+  
+  {
+    
+    id: 2,
+    tit:"",
+    pri:"",
+    adresse:"",
+    deskripsyon: "",
+    imaj:"/image/Screenshot_20251109-155344.png",
+    adminwhatsapp: "Ajoute Anons"
+  },
+  {
+    
+    id: 2,
+    tit: "Kay modèn",
     pri: "$400 / mwa",
-    adresse: "Delmas 75, Pòtoprens",
+    adresse: "",
     deskripsyon: "3 chanm, 2 twalèt, dlo 24/24...",
     imaj: "image/Screenshot_20251201-172702.png",
     adminwhatsapp: "Achte/Negosye"
@@ -14,7 +55,7 @@ const kayList = [
     id: 2,
     tit: "Maison à la campagne",
     pri: "$2000",
-    adresse: ".., Ayiti",
+    adresse: "",
     deskripsyon: "Plizyè chanm",
     imaj: "image/Screenshot_20251109-155526.png",
     adminwhatsapp: "Achte/Negosye"
@@ -32,18 +73,18 @@ const kayList = [
 const terresList = [
       {
     id: 1,
-    tit: "Tè pou lwe nan Camp-Perrin",
+    tit: "Tè pou lwe",
     pri: "$400 / mwa",
-    adresse: " Ayiti/Camp-Perrin",
+    adresse: " Ayiti",
     deskripsyon: "2¹/2 ekta...",
-    imaj: "image/Screenshot_20251109-155633.png",
+    imaj: "/image/Screenshot_20251109-155633.png",
     adminwhatsapp: "Achte/Negosye"
   },
   {
     id: 2,
-    tit: "Tè nan Croix-des-Bouquets",
+    tit: "Tè ",
     pri: "$2000",
-    adresse: "Croix-des-Bouquets, Ayiti",
+    adresse: " Ayiti",
     deskripsyon: "1 ekta tè pou konstwiksyon oswa agrikilti",
     imaj: "image/Screenshot_20251109-155701.png",
     adminwhatsapp: "Achte/Negosye"
@@ -59,26 +100,59 @@ const terresList = [
   }]
   
 const servicesList = [
+  { id: 1,
+    tit: "Mayo (fi)",
+    pri: "",
+    adresse: "",
+    deskripsyon: "",
+    imaj:"/image/Screenshot_20251213-194822.png",
+    adminwhatsapp: "Achte/Negosye"
+  },
+  { id: 1,
+    tit: "Pantalon (Gason)",
+    pri: "",
+    adresse: "",
+    deskripsyon: "",
+    imaj:"/image/Screenshot_20251213-195116.png",
+    adminwhatsapp: "Achte/Negosye"
+  },
+     { id: 1,
+    tit: " Soulye",
+    pri: " 3000 HTG",
+    adresse: "",
+    deskripsyon: "",
+    imaj:"/image/Screenshot_20251213-194332.png",
+    adminwhatsapp: "Achte/Negosye"
+  },
+  
+   { id: 1,
+    tit: " Tenis klasik",
+    pri: " 3000 HTG",
+    adresse: " Haïti/ P-au-P",
+    deskripsyon: "",
+    imaj:"image/Screenshot_20251213-194420.png",
+    adminwhatsapp: "Achte/Negosye"
+  },
   {
     id: 1,
     tit: " Abiman ",
-    pri: " ...",
+    pri: "",
     adresse: " Lòt modèl yo poko disponib ",
     deskripsyon: " Kontakte si w vle antre an kontak ak bòs kouti a...",
-    imaj: "image/IMG-20251201-WA0009.jpg",
+    imaj: "/image/IMG-20251201-WA0009.jpg",
     adminwhatsapp: "Achte/Negosye"
   },
   {
-    id: 2,
+    id: 1,
     tit: "Kamera Profesyonèl",
-    pri: " ... ",
+    pri: "",
     adresse: "... , Ayiti",
-    deskripsyon: " ... ",
-    imaj: "image/Screenshot_20251203-182013.png",
+    deskripsyon: "",
+    imaj: "/image/Screenshot_20251203-182013.png",
     adminwhatsapp: "Achte/Negosye"
   },
   {
-    id: 3,
+    id: 1,
     tit: "Renovasyon kay",
     pri: "$100 / jou",
     adresse: "Pòtoprens",
