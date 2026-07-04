a5524055203dc0a0846ee621cbafb30af7742e2e
radot1530-ai <radot1530@gmail.com>
2026-07-04 13:33:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ import {getAuth, signInWithPopup, onAuthStateChanged, signOut, createUserWithEma
 
 // 🔹 CONFIG FIREBASE
 const firebaseConfig = {
-  apiKey: "AIzaSyAgvH0CpF6tGISpfLw3JWJCT2beBG28wAM",
-  authDomain: "kaylakay-cdf64.firebaseapp.com",
-  databaseURL: "https://kaylakay-cdf64-default-rtdb.firebaseio.com/",
-  projectId: "kaylakay-cdf64",
-  storageBucket: "kaylakay-cdf64.appspot.com",
-  messagingSenderId: "663099511740",
-  appId: "1:663099511740:web:aeb6bddccee9666ff791b9",
-  measurementId: "G-JF9PNTTTG4"
-};
+    apiKey: "AIzaSyB1f26ZYfvHkFWf9x1Zm6bJlrUwbXWWBfk",
+    authDomain: "globalplis-9f740.firebaseapp.com",
+    databaseURL: "https://globalplis-9f740-default-rtdb.firebaseio.com",
+    projectId: "globalplis-9f740",
+    storageBucket: "globalplis-9f740.firebasestorage.app",
+    messagingSenderId: "907235331553",
+    appId: "1:907235331553:web:5b13a1497f857a0fec16a0",
+    measurementId: "G-R91CLS4MY8"
+  };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
