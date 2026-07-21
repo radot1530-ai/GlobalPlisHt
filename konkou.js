025da3e72e507f8d456cb70ab8911ef3660d6c1a
radot1530-ai <radot1530@gmail.com>
2026-07-20 23:13:50 -0400

    Update konkou.js

--- konkou.js
+++ konkou.js
@@ -3,6 +3,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebas
 import { push, set, getDatabase, ref, onValue, runTransaction } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js";
 
 // 🔹 CONFIG FIREBASE
+const firebaseConfig = {
+    apiKey: "AIzaSyB1f26ZYfvHkFWf9x1Zm6bJlrUwbXWWBfk",
+    authDomain: "globalplis-9f740.firebaseapp.com",
+    databaseURL: "https://globalplis-9f740-default-rtdb.firebaseio.com",
+    projectId: "globalplis-9f740",
+    storageBucket: "globalplis-9f740.firebasestorage.app",
+    messagingSenderId: "907235331553",
+    appId: "1:907235331553:web:5b13a1497f857a0fec16a0",
+    measurementId: "G-R91CLS4MY8"
+};
 
 // 🔹 INITIALISATION
 const app = initializeApp(firebaseConfig);
