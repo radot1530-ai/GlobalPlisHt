110d427a3fedac344158b7fa09cf0b083f122f8a
radot1530-ai <radot1530@gmail.com>
2026-07-20 22:33:58 -0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -17,20 +17,7 @@
 
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAgvH0CpF6tGISpfLw3JWJCT2beBG28wAM",
-    authDomain: "kaylakay-cdf64.firebaseapp.com",
-    databaseURL:"https://kaylakay-cdf64-default-rtdb.firebaseio.com/",
-    projectId: "kaylakay-cdf64",
-    storageBucket:"kaylakay-cdf64.appspot.com",
-    messagingSenderId: "663099511740",
-    appId: "1:663099511740:web:aeb6bddccee9666ff791b9",
-    measurementId: "G-JF9PNTTTG4"
-  };
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
+  
 
 // 🔹 ELEMENTS
 const code = document.getElementById("code");
