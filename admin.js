a292f309c7fa5799b0af95d56a95ee237ec5d0a1
radot1530-ai <radot1530@gmail.com>
2026-07-20 23:12:58 -0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -18,6 +18,20 @@
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   
+const firebaseConfig = {
+    apiKey: "AIzaSyB1f26ZYfvHkFWf9x1Zm6bJlrUwbXWWBfk",
+    authDomain: "globalplis-9f740.firebaseapp.com",
+    databaseURL: "https://globalplis-9f740-default-rtdb.firebaseio.com",
+    projectId: "globalplis-9f740",
+    storageBucket: "globalplis-9f740.firebasestorage.app",
+    messagingSenderId: "907235331553",
+    appId: "1:907235331553:web:5b13a1497f857a0fec16a0",
+    measurementId: "G-R91CLS4MY8"
+};
+
+// Initialize Firebase epi EXPORT yo
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
 
 // 🔹 ELEMENTS
 const code = document.getElementById("code");
