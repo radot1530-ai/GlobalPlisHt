3404e66e06c3fda1b6c07170a56a588f967b3a1b
radot1530-ai <radot1530@gmail.com>
2026-07-20 22:35:03 -0400

    Update konkou.js

--- konkou.js
+++ konkou.js
@@ -3,16 +3,6 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebas
 import { push, set, getDatabase, ref, onValue, runTransaction } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js";
 
 // 🔹 CONFIG FIREBASE
-const firebaseConfig = {
-  apiKey: "AIzaSyAgvH0CpF6tGISpfLw3JWJCT2beBG28wAM",
-  authDomain: "kaylakay-cdf64.firebaseapp.com",
-  databaseURL: "https://kaylakay-cdf64-default-rtdb.firebaseio.com/",
-  projectId: "kaylakay-cdf64",
-  storageBucket: "kaylakay-cdf64.appspot.com",
-  messagingSenderId: "663099511740",
-  appId: "1:663099511740:web:aeb6bddccee9666ff791b9",
-  measurementId: "G-JF9PNTTTG4"
-};
 
 // 🔹 INITIALISATION
 const app = initializeApp(firebaseConfig);
